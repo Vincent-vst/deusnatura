e143d2f43edc3ecf30449f3eda284e78b40515ed
“Vincent <vincent.descatoire@gmail.com>
2024-09-19 21:32:29 +0200

    fix SightReading : fix history entry

--- src/pages/SightReading.tsx
+++ src/pages/SightReading.tsx
@@ -1,16 +1,29 @@
 import { useState } from "react";
 import { randomScoreURL } from "./core/SightReadingCore";
 
-
 export default function SightReading() {
-  const [score, setScore] = useState(randomScoreURL());
+
+  //TODO : there has to be a more reacty way to do this
+  function createTag() {
+    if (document.getElementById("iframeScore")) {
+      const elem = document.getElementById("iframeScore");
+      elem?.parentNode?.removeChild(elem);
+    }
+    const nextFrame = document.createElement("iframe");
+    nextFrame.setAttribute("src", randomScoreURL());
+    nextFrame.setAttribute("id", "iframeScore");
+    document.getElementById("frame")?.appendChild(nextFrame);
+  }
+
   return (
     <>
       <h1>Sight reading</h1>
-      <button type="button" onClick={() => setScore(randomScoreURL())}>
-        rnd
+      <button type="button" onClick={() => createTag()}>
+        test
       </button>
-      <iframe src={score} id="iframeScore"></iframe>
+      <div id="frame">
+        <iframe src={randomScoreURL()} id="iframeScore"></iframe>
+      </div>
     </>
   );
 }
